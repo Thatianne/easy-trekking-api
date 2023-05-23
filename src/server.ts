bbb6253b122a909bc5b10a8361a12bb949e4502a
Thatianne Carvalho <thatianne@vizir.com.br>
2023-05-23 17:11:26 -0300

    fix requests when db is initialized

--- src/server.ts
+++ src/server.ts
@@ -12,28 +12,16 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-// app.use((req, res, next) => {
-//   if (!AppDataSource.isInitialized) {
-//     AppDataSource.initialize()
-//       .then(() => {
-//           next();
-//       })
-//       .catch(err => {
-//         console.log(err)
-//       });
-//   }
-// });
-
-// app.use('/', Routes);
-
-AppDataSource.initialize().then(() => {
-  // Routes
-  app.use('/', Routes);
+app.use(async (req, res, next) => {
+  if (!AppDataSource.isInitialized) {
+    await AppDataSource.initialize();
+    next();
+  } else {
+    next();
+  }
 });
+
+app.use('/', Routes);
 
 app.listen(3333, () => 'server running on port 3333'); // for local
 module.exports.handler = serverless(app); // for lambda
